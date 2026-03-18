1ec76a3e6a9254c50f7fc2f68ec42ed9299378a3
LevTrotsky <aspirine220smouk@mail.ru>
2026-03-18 20:43:15 +0400

    Initial commit: dockerized backend with MongoDB

---
+++ app/app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('connected to MongoDB'))
+    .catch(err => console.error(err));
+
+var indexRouter = require('./routes/index');
+var ordersRouter = require('./routes/orders');
+var productsRouter = require('./routes/products');
+var customersRouter = require('./routes/customers');
+var reportsRoutes = require('./routes/reports');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use('/', indexRouter);
+app.use('/api/orders', ordersRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/customers', customersRouter);
+app.use('/reports', reportsRoutes);
+
+module.exports = app;
